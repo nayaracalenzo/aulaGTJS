51ed709acdc817eaeb1da68c73c98a0215255845
nayaracalenzo <queiroz.14@hotmail.com>
2025-04-22 17:18:46 -0300

    introdução do Javascrip

---
+++ index.js
@@ -0,0 +1,40 @@
+// alert("Hello world");
+
+// Declarar uma variável com o seu nome 
+// Declarar uma variável com a sua idade
+// Declarar uma variável com a sua comida favorita e não 
+// atribuir valor
+// Atribuir valor a sua comida favorita
+
+let nome = "Nayara";
+
+
+let comidaFavorita 
+
+console.log(comidaFavorita)
+
+comidaFavorita = "mungunzá salgado";
+
+console.log(comidaFavorita)
+
+// nome = "Julio";
+
+
+nome = prompt('Digite seu nome');
+
+console.log(nome)
+
+//String
+nome = "Nayara"
+let idade = "29"
+console.log(typeof(idade))
+
+// Number
+idade = 30;
+let altura = 1.53
+
+console.log(typeof(altura))
+
+// Boolean (true ou false)
+const desenvolvedoraJavascript = true;  //camelcase
+
